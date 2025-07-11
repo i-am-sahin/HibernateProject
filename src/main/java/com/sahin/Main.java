3bfbe05aa1a5cd85d2af270bc69c8a943ca997ef
Sahin Islam <sahinislam878@gmail.com>
2025-07-11 21:49:44 +0530

    Progress: Completed Failed attempt to save data Lecture from Udemy

--- src/main/java/com/sahin/Main.java
+++ src/main/java/com/sahin/Main.java
@@ -1,9 +1,27 @@
 package com.sahin;
 
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsName("Sahin");
+        s1.setRollNo(626);
+        s1.setsAge(22);
 
+
+        Configuration config = new Configuration();
+        SessionFactory sf = config.buildSessionFactory();
+        Session session = sf.openSession();
+
+
+        session.persist(s1);
+
+
+        System.out.println(s1);
     }
 }
