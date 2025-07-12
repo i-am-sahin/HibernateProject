f21fdfbe85040ab50cf7725ae13140b5670f6962
Sahin Islam <sahinislam878@gmail.com>
2025-07-12 19:09:35 +0530

    Progress: Completed  Fetching data using HQL Lecture from Udemy

--- src/main/java/com/sahin/HQL/Demo.java
+++ src/main/java/com/sahin/HQL/Demo.java
@@ -3,55 +3,39 @@ package com.sahin.HQL;
 
 import com.sahin.HQL.Laptop_From_HQL;
 
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
 
-        Laptop_From_HQL lap = new Laptop_From_HQL();
-        lap.setLid(1);
-        lap.setBrand("Dell");
-        lap.setModel("Inspiron");
-        lap.setRam(8);
 
 
-        Laptop_From_HQL lap2 = new Laptop_From_HQL();
-        lap2.setLid(2);
-        lap2.setBrand("MSI");
-        lap2.setModel("GF63");
-        lap2.setRam(16);
+//        Configuration config = new Configuration();
+//        config.addAnnotatedClass(com.sahin.HQL.Laptop_From_HQL.class);
+//        config.configure();
 
-        Laptop_From_HQL lap3 = new Laptop_From_HQL();
-        lap3.setLid(3);
-        lap3.setBrand("Asus");
-        lap3.setModel("ROG");
-        lap3.setRam(32);
-
-
-        Laptop_From_HQL lap4 = new Laptop_From_HQL();
-        lap4.setLid(4);
-        lap4.setBrand("Asus");
-        lap4.setModel("TUF");
-        lap4.setRam(16);
-
-        Configuration config = new Configuration();
-        config.addAnnotatedClass(com.sahin.HQL.Laptop_From_HQL.class);
-        config.configure();
-
-        SessionFactory sf = config.buildSessionFactory();
+        SessionFactory sf = new Configuration().
+        addAnnotatedClass(com.sahin.HQL.Laptop_From_HQL.class).
+        configure().buildSessionFactory();
         Session s = sf.openSession();
 
-        s.persist(lap);
-        s.persist(lap2);
-        s.persist(lap3);
-        s.persist(lap4);
+//        System.out.println(s.find(Laptop_From_HQL.class,2));
 
 
-        Transaction transaction = s.beginTransaction();
-        transaction.commit();
+        //select * from Laptop_From_HQL where lid=2; --> SQL
+        //from Laptop_From_HQL where lid=2 --> HQL
+
+        Query query = s.createQuery("from Laptop_From_HQL");
+       List<Laptop_From_HQL> laptops =  query.getResultList();
+        System.out.println(laptops);
+
+
         s.close();
 
 
